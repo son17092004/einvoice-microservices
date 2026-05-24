ea5cacaa5c14acc5ffc0a334f6095677d8154dd8
sonnnl <n22dccn170@student.ptithcm.edu.vn>
2026-05-24 15:42:37 +0700

    init lint-staged

--- apps/invoice/src/app/app.service.ts
+++ apps/invoice/src/app/app.service.ts
@@ -3,7 +3,6 @@ import { PORT } from '@common/constants';
 @Injectable()
 export class AppService {
   constructor() {
-    const x = 1;
     console.log(PORT);
   }
   getData(): { message: string } {
